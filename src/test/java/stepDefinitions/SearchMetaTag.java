3598dd3d17985e678788e00049f437ec5b486697
Noerm177 <noerm177@gmail.com>
2020-01-24 11:11:07 -0600

    Matcher y Patterns

--- src/test/java/stepDefinitions/SearchMetaTag.java
+++ src/test/java/stepDefinitions/SearchMetaTag.java
@@ -6,7 +6,6 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import pageObjects.GetQuotePage;
@@ -17,6 +16,8 @@ import pageObjects.YourAccountPage;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SearchMetaTag extends BaseDriver{
     public StoreFrontHomeHeaderMenu home = new StoreFrontHomeHeaderMenu(driver);
@@ -24,9 +25,10 @@ public class SearchMetaTag extends BaseDriver{
     public YourAccountPage accpage = new YourAccountPage(driver);
     public ResultsPage result = new ResultsPage(driver);
     public GetQuotePage gquote = new GetQuotePage(driver);
-    public String MetaRobot = "<meta name=\"robots\" content=\"index,follow\"/>\n";
+    public String MetaRobot = "<meta name=\"robots\" content=\"index,follow\"/>";
     public String source;
     public String MetaString;
+
 
     @Given("^User is on the homepage with (.+)$")
     public void user_is_on_the_homepage_with(String link) throws Throwable {
@@ -44,8 +46,12 @@ public class SearchMetaTag extends BaseDriver{
     @Then("^Search for the robot meta tag name$")
     public void search_for_the_something_name() throws Throwable {
         String source = driver.getPageSource();
-        boolean MetaExist = source.contains(MetaRobot);
-        Assert.assertTrue(MetaExist);
+        String PatternText = ("robots");
+        Pattern patternTag = Pattern.compile(PatternText);
+        Matcher SourceMatch = patternTag.matcher(source);
 
+        while (SourceMatch.find()) {
+            System.out.println(SourceMatch.group());
+        }
     }
 }
