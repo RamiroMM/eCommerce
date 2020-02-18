8f9653b36ce30e70829ae5fca597d198395285c7
Ramiro Mora <hadriel.21@gmail.com>
2020-02-18 17:24:29 -0600

    deleted test-classes files

--- src/test/java/pageObjects/ResultsPage.java
+++ src/test/java/pageObjects/ResultsPage.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import com.framework.util.Locators;
 
 public class ResultsPage {
